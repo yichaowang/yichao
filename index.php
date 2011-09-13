d9f572727f125210fa2418204673bd2f8cd75997
YichaoWang <wenwwyccc@gmail.com>
2011-09-13 03:05:52 -0400

    construction page

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+
+<html lang="en-US">
+<head>
+	<meta content="charset=utf-8">
+	<title>YichaoWang.com</title>
+	<link href='http://fonts.googleapis.com/css?family=Swanky+and+Moo+Moo|Open+Sans:300italic,400' rel='stylesheet' type='text/css'>
+	<link rel='stylesheet' href='css/reset.css' type='text/css' media='all' /> 
+	<link rel='stylesheet' href='css/master.css' type='text/css' media='all' />
+	 
+</head>
+<body>
+	<div id="wrapper">
+		<h1>
+			Yichao Wang
+			<span class="blue">.</span>
+			com
+		</h1>
+		<hr class="w400"/>
+		<h2>
+			Web Developer 
+			<abbr class="and">&amp;</abbr>
+			UI Designer
+		</h2>
+		<div class="construction">
+			Site under construction. Coming soon.
+		</div>   
+
+	</div>
+</body>
+</html>
+
+
+
+
+                                   
