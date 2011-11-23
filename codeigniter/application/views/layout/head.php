bcf15961c84fac10f1a7be7fb8d3e509ad82a82e
YichaoWang <wenwwyccc@gmail.com>
2011-11-23 00:35:10 -0500

    sync

--- codeigniter/application/views/layout/head.php
+++ codeigniter/application/views/layout/head.php
@@ -5,6 +5,16 @@
 	<meta name="description" content="The online portfolio of Yichao Wang, Web Developer and Interactive Designer." />
 	<title>Yichao</title> 
 	<link rel="stylesheet" href="/css/master.css" type="text/css" media="screen">
+	<!--[if lt IE 9]>
+		<script>
+		  var e = ("abbr,article,aside,audio,canvas,datalist,details," +
+		    "figure,footer,header,hgroup,mark,menu,meter,nav,output," +
+		    "progress,section,time,video").split(',');
+		  for (var i = 0; i < e.length; i++) {
+		    document.createElement(e[i]);
+		  }
+		</script>
+	<![endif]-->
 	<script type="text/javascript" src="/js/master.js"></script>
 </head>
 <body>
