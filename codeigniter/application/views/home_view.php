4f65a81f6997761105793600cc05f39b9185087c
YichaoWang <wenwwyccc@gmail.com>
2011-11-23 00:57:13 -0500

    sync

--- codeigniter/application/views/home_view.php
+++ codeigniter/application/views/home_view.php
@@ -7,19 +7,5 @@
 			<a href="#">Please Check Out Some of My Works.</a>
 		</hgroup>
 	</section>
-	<section id="intro">
-		<hgroup class="prefix_2">
-		   	<h1>Hello.<br>My Name is Yichao.</h1>
-			<h2>I am a web developer who like to <br>build interactions to make users happy!</h2>
-			<a href="#">Please Check Out Some of My Works.</a>
-		</hgroup>
-	</section>
-	<section id="intro">
-		<hgroup class="prefix_2">
-		   	<h1>Hello.<br>My Name is Yichao.</h1>
-			<h2>I am a web developer who like to <br>build interactions to make users happy!</h2>
-			<a href="#">Please Check Out Some of My Works.</a>
-		</hgroup>
-	</section>
 </div>
 <?php include('layout/footer.php') ?>
