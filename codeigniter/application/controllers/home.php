81f43f5386e31117e1bf5811e12a2fc419cb3eb9
YichaoWang <wenwwyccc@gmail.com>
2012-02-07 13:48:29 -0800

    update

--- codeigniter/application/controllers/home.php
+++ codeigniter/application/controllers/home.php
@@ -2,7 +2,33 @@
 class Home extends CI_Controller {
 
 	public function index()
-	{
+	{                      
+		/*
+		$this->load->library('session');
+
+				try 
+				{
+					//create or open the database
+					$con = new SQLiteDatabase('private/log.sqlite', 0666, $error);
+				}
+				catch(Exception $e) 
+				{
+					die($error);
+				}     
+				
+				$stm = "CREATE TABLE Friends(Id integer PRIMARY KEY," . 
+					"Name text UNIQUE NOT NULL, Sex text CHECK(Sex IN ('M', 'F')))";
+				$ok = sqlite_exec($con, $stm, $error);
+
+				if (!$ok)
+					die("Cannot execute query. $error");
+
+				echo "Database Friends created successfully";
+
+				sqlite_close($con);  */
+		
+		
+		
 		$this->load->view('home_view');
 	}
 	
--- codeigniter/application/views/layout/footer.php
+++ codeigniter/application/views/layout/footer.php
@@ -3,7 +3,8 @@
 					<div class="grid_12">2012 &copy; Yichao Wang &nbsp; 
 							<a class="mailto" href="#">yichao@yichaowang.com</a> . 
 							<a href="http://www.linkedin.com/pub/yichao-wang/b/79/615">Linked In</a> . 
-							<a href="https://github.com/yichaowang">Git Hub</a>
+							<a href="https://github.com/yichaowang">Git Hub</a> .
+							<a href="#">(517)775-0101</a>
 					</div>
 				</div>
 			</footer> 
